bd4008dc9c9db3bd870eade5d6c81dbbaac90d34
unknown <xai@.(none)>
2009-11-29 00:12:02 +0900

    initial commit

---
+++ lib/Acme/CheckCharactorsOutsideRange.pm
@@ -0,0 +1,41 @@
+package Acme::CheckCharactorsOutsideRange;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::CheckCharactorsOutsideRange -
+
+=head1 SYNOPSIS
+
+  use Acme::CheckCharactorsOutsideRange;
+
+=head1 DESCRIPTION
+
+Acme::CheckCharactorsOutsideRange is
+
+=head1 METHOD
+
+=over
+
+=item
+
+=back
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
